0c7cd1ddc81e53391d93539076a287de7de26d8e
kenjhn <kennedy.ibeh@telesoftas.com>
2021-02-16 16:27:08 +0200

    chore: update steps

---
+++ src/main/java/aoc/Constants.java
@@ -0,0 +1,7 @@
+package aoc;
+
+public class Constants {
+
+    public static final String INPUT_PATH = "input/day";
+    public static final String SOLUTION_PATH = "src/main/resources/solution/day";
+}
--- src/test/java/step/DayFeatureSteps.java
+++ src/test/java/step/DayFeatureSteps.java
@@ -1,12 +1,28 @@
 package step;
 
+import aoc.Day;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.io.IOException;
+import java.util.List;
+
+import static aoc.Constants.INPUT_PATH;
+import static aoc.Constants.SOLUTION_PATH;
+import static java.lang.ClassLoader.getSystemResource;
+import static java.nio.file.Files.readAllLines;
+import static java.nio.file.Path.of;
+import static java.util.Optional.ofNullable;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 public class DayFeatureSteps {
 
     int day;
+    Day implementation;
+    List<String> solutions;
 
     @When("I provide a number {int}")
     public void iProvideANumber(int day) {
@@ -15,17 +31,22 @@ public class DayFeatureSteps {
 
     @Then("check that the input for the number exists")
     public void checkThatTheInputForTheNumberExists() {
-    }
-
-    @And("check that the solution provider has been implemented")
-    public void checkThatTheSolutionProviderHasBeenImplemented() {
+        assertNotNull(getSystemResource(INPUT_PATH + day));
     }
 
     @And("check that the solution has been provided")
-    public void checkThatTheSolutionHasBeenProvided() {
+    public void checkThatTheSolutionHasBeenProvided() throws IOException {
+        solutions = readAllLines(of(SOLUTION_PATH + day));
     }
 
-    @And("check that the solution provided is correct")
+    @And("check that the solution provider has been implemented")
+    public void checkThatTheSolutionProviderHasBeenImplemented() throws ReflectiveOperationException {
+        implementation = (Day) Class.forName("Day" + day).getConstructor().newInstance();
+    }
+
+    @And("check that the solutions provided is correct")
     public void checkThatTheSolutionProvidedIsCorrect() {
+        ofNullable(implementation.getSolution1()).ifPresent(solution -> assertEquals(solution, solutions.get(0)));
+        ofNullable(implementation.getSolution2()).ifPresent(solution -> assertEquals(solution, solutions.get(1)));
     }
 }
