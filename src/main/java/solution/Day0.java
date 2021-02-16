2d6580dd7f4a5a3d09aa1e3df7bac43632710dba
kenjhn <kennedy.ibeh@telesoftas.com>
2021-02-16 16:27:25 +0200

    feat: add Day 0 resources

---
+++ src/main/java/solution/Day0.java
@@ -0,0 +1,16 @@
+package solution;
+
+import aoc.Day;
+
+public class Day0 implements Day {
+
+    @Override
+    public String getSolution1() {
+        return "1";
+    }
+
+    @Override
+    public String getSolution2() {
+        return "2";
+    }
+}
